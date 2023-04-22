a559839b6b230632be4693930b70aa38ecaaa56e
yh-raphael <yhcho.raphael@gmail.com>
2023-04-22 20:07:11 +0900

    dfs - 1012: organic lettuce

---
+++ dfs/1012.cpp
@@ -0,0 +1,87 @@
+#include <iostream>
+#include <vector>
+#include <cstring>
+using namespace std;
+
+int T, M, N, K;
+int map[51][51] = { 0, };           // index: 0~50, 0~50.
+bool check[51][51] = { false, };    // index: 0~50, 0~50.
+vector <int> result;
+
+int dr[4] = {0, 1, 0, -1};      // east, south, west, north.
+int dc[4] = {1, 0, -1, 0};      // east, south, west, north.
+
+void do_dfs (int r, int c)
+{
+    check[r][c] = true;
+
+    // for each direction.
+    for (int i = 0; i < 4; i++)
+    {
+        int nr = r + dr[i];
+        int nc = c + dc[i];
+        if (nr < 0 || nr > M-1 || nc < 0 || nc > N-1) continue;     // out of boundary.
+        if (map[nr][nc] == false) continue;     // no lettuce.
+        if (check[nr][nc] == true) continue;    // is visited already.
+
+        do_dfs (nr, nc);
+    }
+
+    return;
+}
+
+int main (void)
+{
+    ios_base::sync_with_stdio (false);
+    cin.tie (NULL); cout.tie (NULL);
+    cin >> T;
+    // for each test case.
+    for (int iter = 0; iter < T; iter++) {
+        cin >> M >> N >> K;
+
+        // set the map.
+        memset (map, 0, sizeof (map));
+        memset (check, 0, sizeof (check));
+        for (int j = 0; j < K; j++) {
+            int r, c; cin >> r >> c;
+            map[r][c] = 1;
+        }
+
+        // solve.
+        int cnt = 0;
+        for (int i = 0; i < M; i++) {
+            for (int j = 0; j < N; j++) {
+                if (map[i][j] == 1 && check[i][j] == false) {
+
+                    // cout << endl << endl << i << "," << j << endl;
+                    // for (int ii = 0; ii < M; ii ++) {
+                    //     for (int jj = 0; jj < N; jj ++) {
+                    //         cout << check[ii][jj] << ' ';
+                    //     }
+                    //     cout << endl;
+                    // }
+
+                    do_dfs (i, j);  // do the dfs!
+
+                    // cout << endl;
+                    // for (int ii = 0; ii < M; ii ++) {
+                    //     for (int jj = 0; jj < N; jj ++) {
+                    //         cout << check[ii][jj] << ' ';
+                    //     }
+                    //     cout << endl;
+                    // }
+
+                    cnt ++;
+                }
+            }
+        }
+        // store the result.
+        result.push_back (cnt);
+    }
+
+    // print results.
+    for (int iter = 0; iter < T; iter ++) {
+        cout << result[iter] << '\n';
+    }
+    return 0;
+}
