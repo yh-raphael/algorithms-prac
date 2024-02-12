6141443edb03a23abc7e0cd98410b7f0a853cd77
yh-raphael <yhcho.raphael@gmail.com>
2024-02-12 11:51:41 +0900

    2024 bootcamp initial commit

---
+++ 24_bootcamp/Part1/1_strategy/6797.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int N;
+
+void InputData(){
+	cin >> N;
+}
+
+int main(){
+	int ans = -1;
+	InputData();// 입력받는 부분
+
+	// 여기서부터 작성
+
+	cout << ans << endl;// 출력하는 부분
+
+	return 0;
+}
